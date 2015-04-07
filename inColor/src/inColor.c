85ed4170e8f3215b96792495abd14d8dd82e1598
Greg Bartell <gregbartell@users.noreply.github.com>
2015-04-07 17:28:20 -0400

    Get inColor up to date with inSquare

--- inColor/src/inColor.c
+++ inColor/src/inColor.c
@@ -1,5 +1,5 @@
 /*
-  inSquare
+  inColor
 
   Copyright 2015 Theo Browne
   Author: Theo Browne (with help from Greg Bartell)
@@ -21,11 +21,19 @@ static GBitmap *s_background_bitmap;
 GColor top_layer_text_color;
 GColor bottom_layer_text_color;
 GColor background_color;
+double charge;
 
 enum{
-  KEY_MODE = 0x0,
-  KEY_BTV = 0x1,
+  KEY_MODE = 0x0, // Watch display mode
+  KEY_BTV = 0x1, // Vibrate on Bluetooth disconnect
+  KEY_BBB = 0x2, // Bottom Battery Bar
 };
+
+void battery_update_callback(BatteryChargeState charge_state){
+  charge = ( (double)charge_state.charge_percent ) / 100 ;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "charge_percent: %lf", charge);
+  layer_mark_dirty(s_canvas_layer);
+}
 
 static void update_time() {
   
@@ -55,6 +63,12 @@ static void update_time() {
 
 static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
 
+  if(!(charge > 0)){
+    BatteryChargeState charge_state = battery_state_service_peek();
+    battery_update_callback(charge_state);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Charge was undefined, loading now");
+  }
+
   int mode = persist_read_int(KEY_MODE);
   //GRect bounds = layer_get_bounds(this_layer);
 
@@ -63,51 +77,68 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
 
   // Draw the 'stalk'
 
+  GColor topFrameColor;
+  GColor topBackColor;
+  GColor bottomFrameColor;
+  GColor bottomBackColor;
+
   GColor frameColor;
   GColor innerColor;
 
   if(mode == 0){
-    frameColor = GColorWhite;
-    innerColor = GColorBlack;
-  }else if(mode == 1){
-    frameColor = GColorBlack;
-    innerColor = GColorWhite;
-  }else if(mode == 2){
-    //splitSquare, notably different
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_fill_rect(ctx, GRect(0, 0, 144, 84), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_rect(ctx, GRect(14, 6, 116, 78), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_fill_rect(ctx, GRect(17, 9, 110, 75), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_rect(ctx, GRect(0, 85, 144, 84), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_fill_rect(ctx, GRect(14, 84, 116, 78), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_rect(ctx, GRect(17, 84, 110, 75), 0, GCornerNone);
-
-    s_time_font_bottom = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_72));
-
-  }else{
-    frameColor = GColorWhite;
-    innerColor = GColorBlack;
-  }
-
-  if(mode != 2){
-    graphics_context_set_fill_color(ctx, frameColor);
-    graphics_fill_rect(ctx, GRect(14, 6, 116, 156), 0, GCornerNone);
-
-    graphics_context_set_fill_color(ctx, innerColor);
-    graphics_fill_rect(ctx, GRect(17, 9, 110, 150), 0, GCornerNone);
-
+    topFrameColor = GColorWhite;
+    topBackColor = GColorBlack;
+    bottomFrameColor = GColorWhite;
+    bottomBackColor = GColorBlack;
     s_time_font_bottom = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_Light_72));
+  }else if(mode == 1){
+    topFrameColor = GColorBlack;
+    topBackColor = GColorWhite;
+    bottomFrameColor = GColorBlack;
+    bottomBackColor = GColorWhite;
+    s_time_font_bottom = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_Light_72));
+  }else if(mode == 2){
+    topFrameColor = GColorBlack;
+    topBackColor = GColorWhite;
+    bottomFrameColor = GColorWhite;
+    bottomBackColor = GColorBlack;
+    s_time_font_bottom = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_72));
+  }else{
+    topFrameColor = GColorWhite;
+    topBackColor = GColorBlack;
+    bottomFrameColor = GColorWhite;
+    bottomBackColor = GColorBlack;
   }
+
+  //Background colors
+  graphics_context_set_fill_color(ctx, topBackColor);
+  graphics_fill_rect(ctx, GRect(0, 0, 144, 84), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, bottomBackColor);
+  graphics_fill_rect(ctx, GRect(0, 85, 144, 84), 0, GCornerNone);
+
+  //Frame colors
+  graphics_context_set_fill_color(ctx, topFrameColor);
+  graphics_fill_rect(ctx, GRect(14, 6, 116, 78), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, bottomFrameColor);
+  graphics_fill_rect(ctx, GRect(14, 84, 116, 75), 0, GCornerNone);
+
+  //Inner colors
+  graphics_context_set_fill_color(ctx, topBackColor);
+  graphics_fill_rect(ctx, GRect(17, 9, 110, 75), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, bottomBackColor);
+  graphics_fill_rect(ctx, GRect(17, 84, 110, 75), 0, GCornerNone);
+
+  //Bottom edge (and Battery Life)
+  graphics_context_set_fill_color(ctx, bottomFrameColor);
+
+  bool bbb = persist_read_bool(KEY_BBB);
+
+  if(bbb){
+    graphics_fill_rect(ctx, GRect(14, 159, (116 * charge), 3), 0, GCornerNone);
+  }else{
+    graphics_fill_rect(ctx, GRect(14, 159, (116), 3), 0, GCornerNone);
+  }
+
 }
 
 static void bluetooth_connection_callback(bool connected){
@@ -123,6 +154,7 @@ static void in_recv_handler(DictionaryIterator *iterator, void *context)
   //Get Tuple
   Tuple *mode_tuple = dict_find(iterator, KEY_MODE);
   Tuple *btv_tuple = dict_find(iterator, KEY_BTV);
+  Tuple *bbb_tuple = dict_find(iterator, KEY_BBB);
   
   if(mode_tuple){
 
@@ -153,12 +185,20 @@ static void in_recv_handler(DictionaryIterator *iterator, void *context)
   }
 
   if(btv_tuple){
-
       if(strcmp(btv_tuple->value->cstring, "0") == 0)
         {
           persist_write_bool(KEY_BTV, false);
         }else if(strcmp(btv_tuple->value->cstring, "1") == 0){
           persist_write_bool(KEY_BTV, true);
+        }
+  }
+
+  if(bbb_tuple){
+      if(strcmp(bbb_tuple->value->cstring, "0") == 0)
+        {
+          persist_write_bool(KEY_BBB, false);
+        }else if(strcmp(bbb_tuple->value->cstring, "1") == 0){
+          persist_write_bool(KEY_BBB, true);
         }
   }
 }
@@ -268,6 +308,7 @@ static void init() {
   app_message_register_inbox_received(in_recv_handler);
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
   bluetooth_connection_service_subscribe(bluetooth_connection_callback);
+  battery_state_service_subscribe(battery_update_callback);
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
@@ -280,6 +321,7 @@ static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
   bluetooth_connection_service_unsubscribe();
+  battery_state_service_unsubscribe();
 }
 
 int main(void) {
